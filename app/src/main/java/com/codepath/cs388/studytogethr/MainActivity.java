4e7cf3155c3301301486eebc90afbfbdf1394a58
Cristian Teran <cristianteranv@gmail.com>
2021-11-24 13:38:48 -0500

    Login and Sign Up done

--- app/src/main/java/com/codepath/cs388/studytogethr/MainActivity.java
+++ app/src/main/java/com/codepath/cs388/studytogethr/MainActivity.java
@@ -2,7 +2,12 @@ package com.codepath.cs388.studytogethr;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnLogout = findViewById(R.id.btnLogout);
+
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ParseUser.logOut();
+                Intent i = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
