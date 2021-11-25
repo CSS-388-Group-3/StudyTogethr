1bf1b9deda3aeba941a584c0bfd0111fc1b24ed1
kp <kp55@njit.edu>
2021-11-25 14:05:02 -0500

    Profile fragment created

--- app/src/main/java/com/codepath/cs388/studytogethr/MainActivity.java
+++ app/src/main/java/com/codepath/cs388/studytogethr/MainActivity.java
@@ -3,6 +3,7 @@ package com.codepath.cs388.studytogethr;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,21 +12,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.codepath.cs388.studytogethr.fragments.ProfileFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
-    Button btnLogout;
 
     public static final String TAG = "MainActivity";
-
+    final FragmentManager fragmentManager = getSupportFragmentManager();
     private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnLogout = findViewById(R.id.btnLogout);
+
         bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -35,30 +36,24 @@ public class MainActivity extends AppCompatActivity {
                     case R.id.action_classList:
                         Toast.makeText(MainActivity.this, "Class List!", Toast.LENGTH_SHORT).show();
                         //fragment = new ClassListFragment();
+                        fragment = new ProfileFragment();
                         break;
                     case R.id.action_compose:
                         Toast.makeText(MainActivity.this, "Compose Notes!", Toast.LENGTH_SHORT).show();
                         //fragment = new ComposeFragment();
+                        fragment = new ProfileFragment();
                         break;
                     case R.id.action_profile:
                     default:
                         Toast.makeText(MainActivity.this, "Profile!", Toast.LENGTH_SHORT).show();
-                        //fragment = new ProfileFragment();
+                        fragment = new ProfileFragment();
                         break;
                 }
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
                 return true;
             }
         });
         // Set default selection
         bottomNavigationView.setSelectedItemId(R.id.action_classList);
-
-        btnLogout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ParseUser.logOut();
-                Intent i = new Intent(getApplicationContext(), LoginActivity.class);
-                startActivity(i);
-            }
-        });
     }
 }
