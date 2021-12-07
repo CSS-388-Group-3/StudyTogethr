db08841897e591e600c1562c5e6b752d71639da4
kp <kp55@njit.edu>
2021-12-07 01:25:02 -0500

    school email verification [bonus]

--- app/src/main/java/com/codepath/cs388/studytogethr/LoginActivity.java
+++ app/src/main/java/com/codepath/cs388/studytogethr/LoginActivity.java
@@ -63,7 +63,7 @@ public class LoginActivity extends AppCompatActivity {
             public void done(ParseUser user, ParseException e) {
                 if ( e != null ) {
                     Log.e(TAG, "error with login", e);
-                    Toast.makeText(LoginActivity.this, "Incorrect credentials", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(LoginActivity.this, "Try Again or Verify Email", Toast.LENGTH_SHORT).show();
                     return;
                 }
                 Toast.makeText(LoginActivity.this, "Welcome!", Toast.LENGTH_SHORT).show();
